05d4c5b963ac93c89b37c7e32793d82d60f3d916
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-12-03 15:27:43 -0300

    agrego el proyecto de android

---
+++ RazasYPelajesTettamanti/app/src/main/java/com/example/santi/razasypelajestettamanti/RazasYPelajes.java
@@ -0,0 +1,13 @@
+package com.example.santi.razasypelajestettamanti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RazasYPelajes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_razas_ypelajes);
+    }
+}
