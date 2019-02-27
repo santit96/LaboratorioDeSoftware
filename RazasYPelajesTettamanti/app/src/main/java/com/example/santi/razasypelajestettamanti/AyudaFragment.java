b7d035b6148f427a96c703c0b31cbcdce7e08173
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-02-27 00:37:08 -0300

    FINISH razas y pelajes

--- RazasYPelajesTettamanti/app/src/main/java/com/example/santi/razasypelajestettamanti/AyudaFragment.java
+++ RazasYPelajesTettamanti/app/src/main/java/com/example/santi/razasypelajestettamanti/AyudaFragment.java
@@ -3,18 +3,22 @@ package com.example.santi.razasypelajestettamanti;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.TextView;
 
 public class AyudaFragment extends DialogFragment {
 
     private AlertDialog.Builder builder;
     private int textId;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         this.builder = new AlertDialog.Builder(getActivity());
